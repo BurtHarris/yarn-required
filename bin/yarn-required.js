744ddc43a9f084830cea74abd4edb68c71b987cc
Burt Harris <Burt.Harris_github@azxs.33mail.com>
2017-12-17 15:52:10 -0800

    Initial code

---
+++ bin/yarn-required.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const process = require('process');
+const npmlog = require('npmlog')
+const exepath = process.env.npm_execpath;
+
+if (exepath && exepath.includes('yarn')) return 0;
+
+npmlog.error("error - this script must be run from within yarn!");
+if (exepath) npmlog.info('npm_execpath: ', exepath);
+return 1;
